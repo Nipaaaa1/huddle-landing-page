3242b4fe79f8a3ac7a3bb5789006984752cc9262
Nipaaaa1 <Frostone224@gmail.com>
2024-01-18 18:42:20 +0700

    Desktop Design

--- src/App.jsx
+++ src/App.jsx
@@ -2,30 +2,33 @@ import SocialMedia from "./components/SocialMedia";
 
 const App = () => {
   return (
-    <main className="flex h-svh w-full flex-col bg-desktop bg-cover bg-no-repeat">
+    <main className="flex h-svh w-full flex-col bg-desktop bg-cover bg-no-repeat p-16">
       <nav>
-        <img src="/images/logo.svg" alt="Huddle Logo" />
+        <img className="h-14 w-max" src="/images/logo.svg" alt="Huddle Logo" />
       </nav>
-      <section>
+      <section className="flex h-full w-full items-center justify-between gap-16 pt-24  ">
         <img
+          className="h-full w-max"
           src="/images/illustration-mockups.svg"
           alt="Illustration Mockups"
         />
-        <div>
-          <h1>Build The Community Your Fans Will Love</h1>
-          <p>
+        <div className="flex w-max -translate-y-32 flex-col gap-8 font-poppins">
+          <h1 className="w-5/6 text-balance text-4xl font-semibold leading-snug text-white">
+            Build The Community Your Fans Will Love
+          </h1>
+          <p className="w-2/3 text-pretty text-white">
             Huddle re-imagines the way we build communities. You have a voice,
             but so does your audience. Create connections with your users as you
             engage in genuine discussion.
           </p>
           <a href="#">
-            <button>register</button>
+            <button className="rounded-full bg-white px-16 py-4 text-violet shadow-xl">
+              Register
+            </button>
           </a>
         </div>
       </section>
-      <footer>
-        <SocialMedia />
-      </footer>
+      <SocialMedia />
     </main>
   );
 };
