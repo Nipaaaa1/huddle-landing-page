51883e56228b75fbb521eca89cf1d2609322dad2
Nipaaaa1 <Frostone224@gmail.com>
2024-01-18 16:00:28 +0700

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        openSans: ['Open Sans', 'sans-serif'],
+        poppins: ['Poppins', 'sans-serif']
+      },
+      colors: {
+        violet: 'hsl(257, 40%, 49%)',
+        softMagenta: 'hsl(300, 69%, 71%)',
+      }
+    },
+  },
+  plugins: [],
+}
