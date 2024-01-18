7a4c9797b730a05f5661d01f65b592754ff4d8e5
Nipaaaa1 <Frostone224@gmail.com>
2024-01-18 16:55:32 +0700

    Desktop Design

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        openSans: ['Open Sans', 'sans-serif'],
-        poppins: ['Poppins', 'sans-serif']
+        openSans: ["Open Sans", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"],
       },
       colors: {
-        violet: 'hsl(257, 40%, 49%)',
-        softMagenta: 'hsl(300, 69%, 71%)',
-      }
+        violet: "hsl(257, 40%, 49%)",
+        softMagenta: "hsl(300, 69%, 71%)",
+      },
+      backgroundImage: {
+        desktop: "url('/images/bg-desktop.svg')",
+        mobile: "url('/images/bg-mobile.svg')",
+      },
     },
   },
   plugins: [],
-}
+};
